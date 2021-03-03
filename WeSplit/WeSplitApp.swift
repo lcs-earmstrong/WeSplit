e700ad2feeda085114e894caadb852c9d286f14d
Evan Armstrong <earmstrong@lcs.on.ca>
2021-03-03 11:29:27 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Evan Armstrong on 2021-03-03.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
